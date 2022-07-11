eee4dd76ca3441ba269ae1ab32117152e56d841e
Aigul <sharipova.aig@gmail.com>
2022-07-11 19:30:15 -0400

    if not numbers 1 to 20

--- script.js
+++ script.js
@@ -5,4 +5,12 @@ button.addEventListener('click', play);
 
 function play() {
   const userNumber = document.querySelector('#gues').value; 
+  if (userNumber < 1 || userNumber > 20) {
+    Swal.fire({
+      icon: 'error',
+      title: 'Oops...',
+      text: 'Write numbers 1 to 20!',
+      footer: '<a href="">Why do I have this issue?</a>'
+    })
+  }
 }
