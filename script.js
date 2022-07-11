64bc1270a9656ab5d8111f9e42d29c4acce74117
Aigul <sharipova.aig@gmail.com>
2022-07-11 19:36:00 -0400

    if function

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
-const inputtext = document.querySelector('.#gues');
+const inputText = document.querySelector('#gues');
 const button = document.querySelector('#btn');
 
 button.addEventListener('click', play);
 
 function play() {
+
   const userNumber = document.querySelector('#gues').value; 
   if (userNumber < 1 || userNumber > 20) {
     Swal.fire({
       icon: 'error',
       title: 'Oops...',
       text: 'Write numbers 1 to 20!',
-      footer: '<a href="">Why do I have this issue?</a>'
     })
   }
+  
 }
