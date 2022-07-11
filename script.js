91594d6557e8492d0a1a9a35d2cfeeb40ac4e4c3
Aigul <sharipova.aig@gmail.com>
2022-07-11 19:16:57 -0400

    function play

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ const inputtext = document.querySelector('.#gues');
 const button = document.querySelector('#btn');
 
 button.addEventListener('click', play);
+
+function play() {
+  const userNumber = document.querySelector('#gues').value; 
+}
